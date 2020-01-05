3ff9b7a07ca2cb9d4192756e488911294f8c7b41
romanaverkin <romanaverkin@gmail.com>
2020-01-05 22:31:57 +0200

    writing train func

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -4,10 +4,11 @@ import scipy.special
 
 class NeuralNetwork:
 
-    def __init__(self, input_nodes, hidden_nodes, output_nodes):
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
         self.hidden_nodes = hidden_nodes
         self.input_nodes = input_nodes
         self.output_nodes = output_nodes
+        self._learning_rate = learning_rate
 
         self.wih = numpy.random.normal(0.0, pow(self.input_nodes, -0.5), (self.hidden_nodes, self.input_nodes))
         self.who = numpy.random.normal(0.0, pow(self.hidden_nodes, -0.5), (self.output_nodes, self.hidden_nodes))
@@ -27,3 +28,14 @@ class NeuralNetwork:
         hidden_inputs = numpy.dot(self.wih, inputs)
         hidden_outputs = self._activation_function(hidden_inputs)
         final_outputs = numpy.dot(self.who, hidden_outputs)
+
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        self.who += self._learning_rate * numpy.dot(
+            (output_errors*final_outputs*(1.0-final_outputs)), numpy.transpose(hidden_outputs)
+        )
+
+        self.wih += self._learning_rate * numpy.dot(
+            (hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs)
+        )
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from src.NeuralNetwork import NeuralNetwork
 
-nn = NeuralNetwork(3, 3, 3)
+nn = NeuralNetwork(3, 3, 3, 0.3)
 response = nn.query([1.0, 0.5, -1.5])
 
 print(response)
