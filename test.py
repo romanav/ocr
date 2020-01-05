81c4552a4bb13699cb0b63e5ab91a9dd75fdf378
romanaverkin <romanaverkin@gmail.com>
2020-01-05 23:26:55 +0200

    training data / read and plot

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
+import matplotlib.pyplot
+import numpy
+
 from src.NeuralNetwork import NeuralNetwork
 
 nn = NeuralNetwork(3, 3, 3, 0.3)
 response = nn.query([1.0, 0.5, -1.5])
 
-print(response)
+with open('resources/mnist_train_100.csv', 'r') as data_file:
+    data_list = data_file.readlines()
+
+all_values = data_list[0].split(',')
+image_array = numpy.asfarray(all_values[1:]).reshape((28, 28))
+matplotlib.pyplot.imshow(image_array, cmap='Greys', interpolation='None')
